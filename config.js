159da53b84bfa330a277ef5bed626a154a6d9655
C D H <chamod.binance.lk@gmail.com>
2025-11-29 16:52:02 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const BOT_TOKEN = process.env.BOT_TOKEN || ''; 
+const OWNER_ID = process.env.OWNER_ID || ''; 
+const RAILWAY_API_URL = process.env.RAILWAY_API_URL || 'https://web-production-60f63.up.railway.app/api/template';
